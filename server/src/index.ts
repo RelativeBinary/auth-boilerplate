77a0496fe393e3df75fa82ea7bfd46a7ea02d229
RelativeBinary <patrick.bornay@gmail.com>
2025-06-22 16:33:47 +1000

    add supabase connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { config } from "dotenv";
-
+import supabase from "./supabaseClient";
 config();
 const app = express();
 const PORT = process.env.PORT || "ERROR NO PORT DEFINED";
 
-app.get('/', (_, res) => {
+app.get("/", async (_, res) => {
+  const { data } = await supabase.from("tasks").select();
+  console.log("number of tasks", data?.length);
   res.send("<h1> hello world </h1>");
 });
 
